fc8aa63e60ffdd9f67c850736500cbc01835c5e9
alex <www.aivanov@gmail.com>
2019-12-03 19:03:27 +0300

    argparser tests

--- tests/test_utils_args_parser.py
+++ tests/test_utils_args_parser.py
@@ -1,4 +1,32 @@
+from argparse import Namespace
 import unittest
 
 from utils.args_parser import create_parser
 
+class CommandLineTestCase(unittest.TestCase):
+    @classmethod
+    def setUpClass(cls):
+        parser = create_parser()
+        cls.parser = parser
+
+
+class ConverterTestCase(CommandLineTestCase):
+    def test_with_empty_args(self):
+        with self.assertRaises(SystemExit):
+            self.parser.parse_args(['--input_file', '--window_size'])
+
+    def test_with_two_args_wrong_window_type_float(self):
+        with self.assertRaises(SystemExit):
+            self.parser.parse_args(['--input_file', 'input.json', '--window_size', '10.0'])
+
+    def test_with_two_args_wrong_window_type_str(self):
+        with self.assertRaises(SystemExit):
+            self.parser.parse_args(['--input_file', 'input.json', '--window_size', 'abc'])
+
+    def test_with_two_args_all_ok(self):
+        self.assertEqual(self.parser.parse_args(['--input_file', 'input.json', '--window_size', '10']),
+                         Namespace(input_file='input.json', window_size=10))
+
+
+if __name__ == '__main__':
+    unittest.main()
